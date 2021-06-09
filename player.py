cf46cb2fc4a089d7eeed6bccf008e02b58097e10
MoRadman11 <moramdan481@gmail.com>
2021-06-09 06:26:17 +0200

    Define Human Player Class

--- player.py
+++ player.py
@@ -16,3 +16,11 @@ class Player():
             print( 'AI turn')
         else:
             print( 'PLAYER  {} turn  '.format(self.player_turn))
+
+class HumanPlayer(Player):
+    type = False
+    def make_move(self,board):
+        move = int(input("Enter valid  Move: \n ").replace(" ",""))
+        while not board.validate_move(move):
+            move =int( input("Enter valid  Move: \n ").replace(" ",""))
+        return  -1,int(move)
